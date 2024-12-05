3418aa96ad50fe0f12f75a116b3119c7e5bc1356
Schaeffer <philippeschaeffer67@gmail.com>
2024-12-05 20:53:14 +0100

    Création Entités + Enums dans Domain

---
+++ TodoList/TodoList.Domain/Entities/TodoList.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoList.Domain.Entities
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = new();
+    }
+}
---
+++ TodoList/TodoList.Domain/Enums/PriorityLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoList.Domain.Enums
+{
+    public enum PriorityLevel
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+}
