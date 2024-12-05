5470d6d29c9277dfd8034c2c5a65d36952021615
Schaeffer <philippeschaeffer67@gmail.com>
2024-12-05 20:55:11 +0100

    Création Interfaces ITodoItemRepository dans Application

---
+++ TodoList/TodoList.Application/Interfaces/ITodoItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using TodoList.Domain.Entities;
+
+namespace TodoList.Application.Interfaces
+{
+    public interface ITodoItemRepository
+    {
+        Task<IEnumerable<TodoItem>> GetAllAsync();
+        Task<IEnumerable<TodoItem>> GetAsync(Func<bool,IEnumerable<TodoItem>> predicat);
+        Task<TodoItem> GetSingleAsync(Func<bool,TodoItem> predicat);
+        Task<IEnumerable<TodoItem>> GetByTodoListIdAsync(int todoListId);
+        Task<TodoItem> GetByIdAsync(int id);
+
+        Task AddAsync(TodoItem item);
+        Task UpdateAsync(TodoItem item);
+        Task DeleteByIdAsync(int id);
+    }
+}
